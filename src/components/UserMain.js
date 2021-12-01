1cd20a591e6521d3f3a3810f24bfa88f33d5f493
annpermi <perminova.anna85@gmail.com>
2021-12-01 15:09:37 -0500

    box-shadow

--- src/components/UserMain.js
+++ src/components/UserMain.js
@@ -4,6 +4,23 @@ import "./userMain.css";
 const UserInfo = ({ selectedUser }) => {
   const { city, street, state, zip, phone, createdAt, lastLoggedIn } =
     selectedUser;
+
+  const getDate = (dataStr) => {
+    const date = new Date(dataStr);
+    const time = date.toLocaleString();
+    return time.slice(0, time.length - 6) + time.slice(-3);
+  };
+
+  const formatPhoneNumber = (phoneNumberString) => {
+    //https://www.codegrepper.com/code-examples/javascript/javascript+function+to+format+phone+number
+    var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      return "(" + match[1] + ") " + match[2] + "-" + match[3];
+    }
+    return null;
+  };
+
   return (
     <div className="card_main">
       <div>
@@ -14,28 +31,18 @@ const UserInfo = ({ selectedUser }) => {
       </div>
       <div>
         <p>Phone</p>
-        <p>{phone}</p>
+        <p>{formatPhoneNumber(phone)}</p>
       </div>
       <div>
         <p>Created At</p>
-        <p>{createdAt}</p>
+        <p>{getDate(createdAt)}</p>
       </div>
       <div>
         <p>Last Logged In</p>
-        <p>{lastLoggedIn}</p>
+        <p>{getDate(lastLoggedIn)}</p>
       </div>
     </div>
   );
 };
 
 export default UserInfo;
-// 1970-01-01T06:01:00.000Z
-// 1/1/70 12:01 AM
-
-const getDate = (dataStr) => {
-  const date = new Date(dataStr);
-  const time = date.toLocaleString();
-  return time;
-};
-
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
